a7e2f114c88f4207dc7d938b0717c64d3566e3a1
milrayitas <63482672+milrayitas@users.noreply.github.com>
2021-02-21 14:47:12 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,61 +1,25 @@
-# import websocket
-from websocket import create_connection
-import json
-import pandas as pd
-# import psutil
+import numpy as np
 import matplotlib.pyplot as plt
-import time
-import datetime as dt
-from datetime import datetime , date
-import pytz
-import plotly
-import plotly.graph_objects as go
-from IPython.display import clear_output
+import matplotlib.animation as animation
 
+fig, ax = plt.subplots()
 
-ws = create_connection('wss://api.hitbtc.com/api/2/ws')      
-ws.send('{"method":"subscribeTicker" , "params":{"symbol":"BTCUSD"}}') 
-ws.recv()
+max_x = 5
+max_rand = 10
 
-def graphic_online():
-  tz_BA = pytz.timezone('America/Buenos_Aires')
-  ahora=datetime.now(tz_BA).time()
-  ahoraFormat = ahora.strftime("%H:%M")
-  print(ahoraFormat)
-  end_MervalCI = datetime.strptime('15:57', "%H:%M").time()
-  start_MervalCI = datetime.strptime('11:00', "%H:%M").time()
-  Check_hora = False #@param {type:"boolean"}
-  
-  if Check_hora == True:
-    print(start_MervalCI, ahoraFormat, end_MervalCI)
-    if (start_MervalCI < ahora < end_MervalCI) != True:
-      print('Mercado Cerrado')
-      return
-    else:
-      print("Dentro del horario del mercado")
-      print('')
-      pass
-  else:
-    print('Estamos en modo prueba')
-    pass 
-  
-  recibido=[]
-  oldtime = time.time()
-  while True:
-    try:
-      datos=json.loads(ws.recv())['params']#aqui poner el websocket que corrponda
-      recibido.append(datos)
-    except:
-      pass
-    if time.time() - oldtime < 10:#poner cada 60, antes es al pedo
-      pass
-    else:
-      a=pd.DataFrame(recibido)
-      oldtime = time.time()
-      clear_output()
-       
-      fig= go.Figure(data=[go.Scatter(x=a['timestamp'], y=a['last'])])
-      fig.show()
+x = np.arange(0, max_x)
+ax.set_ylim(0, max_rand)
+line, = ax.plot(x, np.random.randint(0, max_rand, max_x))
 
- 
-b=graphic_online() 
+def init():  # give a clean slate to start
+    line.set_ydata([np.nan] * len(x))
+    return line,
+
+def animate(i):  # update the y values (every 1000ms)
+    line.set_ydata(np.random.randint(0, max_rand, max_x))
+    return line,
+
+ani = animation.FuncAnimation(
+    fig, animate, init_func=init, interval=1000, blit=True, save_count=10)
+
+plt.show()
