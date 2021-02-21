597257e8ae1a5feea2cf5083436101acf5f5b71e
milrayitas <63482672+milrayitas@users.noreply.github.com>
2021-02-21 14:41:14 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,60 @@
-
-import numpy as np
+# import websocket
+from websocket import create_connection
+import json
+import pandas as pd
+# import psutil
 import matplotlib.pyplot as plt
+import time
+import datetime as dt
+from datetime import datetime , date
+import pytz
+import plotly.graph_objects as go
+from IPython.display import clear_output
 
-m = 100
-n = 100
-matrix = np.random.normal(0,1,m*n).reshape(m,n)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.ion()
+ws = create_connection('wss://api.hitbtc.com/api/2/ws')      
+ws.send('{"method":"subscribeTicker" , "params":{"symbol":"BTCUSD"}}') 
+ws.recv()
 
-fig.show()
-fig.canvas.draw()
+def graphic_online():
+  tz_BA = pytz.timezone('America/Buenos_Aires')
+  ahora=datetime.now(tz_BA).time()
+  ahoraFormat = ahora.strftime("%H:%M")
+  print(ahoraFormat)
+  end_MervalCI = datetime.strptime('15:57', "%H:%M").time()
+  start_MervalCI = datetime.strptime('11:00', "%H:%M").time()
+  Check_hora = False #@param {type:"boolean"}
+  
+  if Check_hora == True:
+    print(start_MervalCI, ahoraFormat, end_MervalCI)
+    if (start_MervalCI < ahora < end_MervalCI) != True:
+      print('Mercado Cerrado')
+      return
+    else:
+      print("Dentro del horario del mercado")
+      print('')
+      pass
+  else:
+    print('Estamos en modo prueba')
+    pass 
+  
+  recibido=[]
+  oldtime = time.time()
+  while True:
+    try:
+      datos=json.loads(ws.recv())['params']#aqui poner el websocket que corrponda
+      recibido.append(datos)
+    except:
+      pass
+    if time.time() - oldtime < 10:#poner cada 60, antes es al pedo
+      pass
+    else:
+      a=pd.DataFrame(recibido)
+      oldtime = time.time()
+      clear_output()
+       
+      fig= go.Figure(data=[go.Scatter(x=a['timestamp'], y=a['last'])])
+      fig.show()
 
-for i in range(0,100):
-    ax.clear()
-    ax.plot(matrix[i,:])
-    fig.canvas.draw()
+ 
+b=graphic_online() 
